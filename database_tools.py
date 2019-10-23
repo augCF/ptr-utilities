801eef2f5560cbc23d42d6e8b250e51aec4f54e6
augCF <christoph.fetzer@gmail.com>
2019-10-23 22:50:46 +0200

    edit of python file

---
+++ database_tools.py
@@ -0,0 +1,15 @@
+
+
+
+class InitTools:
+    def __init__(self):
+        pass
+
+    def timestamp_fill(self, start_year=2000, last_year=2001):
+        '''
+        makes blablabla
+        :param start_year: first years
+        :param last_year: last year to be filled
+        :return: filled database
+        '''
+        pass
