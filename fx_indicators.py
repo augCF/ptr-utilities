5df1780bcbf520be3d6ebcff093b4bd0f2f38578
Home <christoph.fetzer@gmail.com>
2019-11-01 21:13:06 +0100

    indicators now utilize np.around, minor bugfixes

--- fx_indicators.py
+++ fx_indicators.py
@@ -1,17 +1,20 @@
 from database_tools import InitTools
+import pandas as pd
+import numpy as np
+import time
 
 
 class DatabaseIndicators:
     '''
     Indicators to fill database with. Each Indicator inputs forex pair name, timestamp ID. Outputs Indicator value
-    for given timestamp ID.
+    for given timestamp IDs.
     '''
-    def __init__(self, pairname, windowsize, timestamp_ids):
+    def __init__(self, pairname, n_periods, timestamp_ids):
         self.InitToolsObject = InitTools()
         self.conn = self.InitToolsObject.create_connection()
         self.cursor = self.conn.cursor()
         self.pairname = pairname
-        self.windowsize = windowsize
+        self.n_periods = n_periods
         self.timestamp_ids = timestamp_ids
 
 
@@ -25,27 +28,74 @@ class DatabaseIndicators:
 
         try:
             self.cursor.execute(f"""SELECT {self.pairname}_bidopen, {self.pairname}_bidclose FROM fx_data 
-                                WHERE fx_timestamp_id BETWEEN {self.timestamp_ids[0]} AND {self.timestamp_ids[-1]}""")
+                                WHERE fx_timestamp_id BETWEEN {self.timestamp_ids[0]} AND {self.timestamp_ids[-1]}
+                                ORDER BY fx_timestamp_id ASC""")
             open_close_values = self.cursor.fetchall()
 
             output = []
             for i in range(len(self.timestamp_ids)):
-                if open_close_values[i][0] > open_close_values[i][1]:
+                if float(open_close_values[i][0]) > float(open_close_values[i][1]):
                     output.append(0)
-                elif open_close_values[i][0] == open_close_values[i][1]:
+                elif float(open_close_values[i][0]) == float(open_close_values[i][1]):
                     output.append(1)
-                elif open_close_values[i][0] < open_close_values[i][1]:
+                elif float(open_close_values[i][0]) < float(open_close_values[i][1]):
                     output.append(2)
             return output
 
         except:
             pass
 
-    def rsi(self):
+    def absolute_body_size(self):
+        try:
+            self.cursor.execute(f"""SELECT {self.pairname}_bidopen, {self.pairname}_bidclose FROM fx_data 
+                                WHERE fx_timestamp_id BETWEEN {self.timestamp_ids[0]} AND {self.timestamp_ids[-1]}
+                                ORDER BY fx_timestamp_id ASC""")
+            open_close_values = self.cursor.fetchall()
+
+            output = []
+            for i in range(len(self.timestamp_ids)):
+                output.append(np.around(abs(float(open_close_values[i][0]) - float(open_close_values[i][1])), decimals=5))
+            return output
+
+        except:
+            print("Error at absolute body size")
+
+
+    def cutler_rsi(self):
         '''
-        Welles Wilders
+        Variation of Welles Wilders RSI. Doesnt consider previous RSI Values (no smoothing).
         :return:
         '''
+
+        self.cursor.execute(f"""SELECT {self.pairname}_up_down_1, {self.pairname}_absolute_body_size_1 FROM fx_data
+                            WHERE fx_timestamp_id BETWEEN {self.timestamp_ids[0] - (self.n_periods - 1)}
+                            AND {self.timestamp_ids[-1]}
+                            ORDER BY fx_timestamp_id ASC""")
+        fetch = self.cursor.fetchall()
+        output = []
+        for i in range(len(self.timestamp_ids) - 1):
+            ups = 0
+            downs = 0
+            for j in range(self.n_periods):
+                if fetch[i+j][0] == 2:
+                    ups += float(fetch[i+j][1])
+                elif fetch[i+j][0] == 0:
+                    downs += float(fetch[i+j][1])
+                else:
+                    ups += 0.0000001
+                    downs += 0.0000001
+            ups /= self.n_periods
+            downs /= self.n_periods
+            rs = ups/downs
+            rsi = 100-(100 / (1 + rs))
+            output.append(float(np.around(rsi, decimals=3)))
+
+        return output
+
+
+
+
+
 
 
 
