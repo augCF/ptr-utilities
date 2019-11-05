4cbc2e4db110572c1a6fe302a1d7b801e6888a44
Home <christoph.fetzer@gmail.com>
2019-11-05 21:11:19 +0100

    Indicators updated

--- fx_indicators.py
+++ fx_indicators.py
@@ -9,13 +9,14 @@ class DatabaseIndicators:
     Indicators to fill database with. Each Indicator inputs forex pair name, timestamp ID. Outputs Indicator value
     for given timestamp IDs.
     '''
-    def __init__(self, pairname, n_periods, timestamp_ids):
+    def __init__(self, pairname, n_periods, valid_ids): # timestamp_ids):
         self.InitToolsObject = InitTools()
         self.conn = self.InitToolsObject.create_connection()
         self.cursor = self.conn.cursor()
         self.pairname = pairname
         self.n_periods = n_periods
-        self.timestamp_ids = timestamp_ids
+        # self.timestamp_ids = timestamp_ids
+        self.valid_ids = valid_ids
 
 
     def up_down(self):
@@ -26,34 +27,37 @@ class DatabaseIndicators:
         :return: Integer with value 0, 1, or 2.
         '''
 
-        try:
-            self.cursor.execute(f"""SELECT {self.pairname}_bidopen, {self.pairname}_bidclose FROM fx_data 
-                                WHERE fx_timestamp_id BETWEEN {self.timestamp_ids[0]} AND {self.timestamp_ids[-1]}
-                                ORDER BY fx_timestamp_id ASC""")
-            open_close_values = self.cursor.fetchall()
+        self.cursor.execute(f"""SELECT {self.pairname}_bidopen, {self.pairname}_bidclose FROM fx_data 
+                            WHERE {self.pairname}_valid_id BETWEEN {self.valid_ids[0]} AND {self.valid_ids[-1]}
+                            ORDER BY fx_timestamp_id ASC""")
 
-            output = []
-            for i in range(len(self.timestamp_ids)):
-                if float(open_close_values[i][0]) > float(open_close_values[i][1]):
-                    output.append(0)
-                elif float(open_close_values[i][0]) == float(open_close_values[i][1]):
-                    output.append(1)
-                elif float(open_close_values[i][0]) < float(open_close_values[i][1]):
-                    output.append(2)
-            return output
+        open_close_values = np.array(self.cursor.fetchall())
 
-        except:
-            pass
+        output = []
+        for i in range(len(self.valid_ids)):
+            if float(open_close_values[i][0]) > float(open_close_values[i][1]):
+                output.append(0)
+            elif float(open_close_values[i][0]) == float(open_close_values[i][1]):
+                output.append(1)
+            elif float(open_close_values[i][0]) < float(open_close_values[i][1]):
+                output.append(2)
+        return output
+
 
     def absolute_body_size(self):
         try:
+            # self.cursor.execute(f"""SELECT {self.pairname}_bidopen, {self.pairname}_bidclose FROM fx_data
+            #                     WHERE fx_timestamp_id BETWEEN {self.timestamp_ids[0]} AND {self.timestamp_ids[-1]}
+            #                     ORDER BY fx_timestamp_id ASC""")
+
             self.cursor.execute(f"""SELECT {self.pairname}_bidopen, {self.pairname}_bidclose FROM fx_data 
-                                WHERE fx_timestamp_id BETWEEN {self.timestamp_ids[0]} AND {self.timestamp_ids[-1]}
+                                WHERE {self.pairname}_valid_id BETWEEN {self.valid_ids[0]} AND {self.valid_ids[-1]}
                                 ORDER BY fx_timestamp_id ASC""")
+
             open_close_values = self.cursor.fetchall()
 
             output = []
-            for i in range(len(self.timestamp_ids)):
+            for i in range(len(self.valid_ids)):
                 output.append(np.around(abs(float(open_close_values[i][0]) - float(open_close_values[i][1])), decimals=5))
             return output
 
@@ -68,12 +72,12 @@ class DatabaseIndicators:
         '''
 
         self.cursor.execute(f"""SELECT {self.pairname}_up_down_1, {self.pairname}_absolute_body_size_1 FROM fx_data
-                            WHERE fx_timestamp_id BETWEEN {self.timestamp_ids[0] - (self.n_periods - 1)}
-                            AND {self.timestamp_ids[-1]}
+                            WHERE {self.pairname}_valid_id BETWEEN {self.valid_ids[0] - self.n_periods}
+                            AND {self.valid_ids[-1]}
                             ORDER BY fx_timestamp_id ASC""")
-        fetch = self.cursor.fetchall()
+        fetch = np.array(self.cursor.fetchall())
         output = []
-        for i in range(len(self.timestamp_ids) - 1):
+        for i in range(len(fetch) - (self.n_periods - 1)):
             ups = 0
             downs = 0
             for j in range(self.n_periods):
@@ -81,9 +85,8 @@ class DatabaseIndicators:
                     ups += float(fetch[i+j][1])
                 elif fetch[i+j][0] == 0:
                     downs += float(fetch[i+j][1])
-                else:
-                    ups += 0.0000001
-                    downs += 0.0000001
+            if downs == 0:
+                downs += 0.0000001
             ups /= self.n_periods
             downs /= self.n_periods
             rs = ups/downs
@@ -92,8 +95,8 @@ class DatabaseIndicators:
 
         return output
 
-
-
+    def simple_moving_average(self):
+        pass
 
 
 
