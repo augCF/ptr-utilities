581fd3b5bd52a4e063a2596ec94eb632b7d270b6
Home <christoph.fetzer@gmail.com>
2019-11-14 00:10:44 +0100

    initial version

--- trading_strategies.py
+++ trading_strategies.py
@@ -1,8 +1,19 @@
+import fx_indicators
+
 
 class Teststrategy:
     def __init__(self):
-            self.x_width = 10
+            self.x_width = 80
 
     def decide(self, minute_bar_array, equity, balance):
-        x = "s"
+        rsi_30 = fx_indicators.LiveIndicators(minute_bar_array).cutler_rsi(70)
+        if sum(rsi_30)/(self.x_width - 70) > 70:
+            x = "s"
+        # elif sum(rsi_30)/(self.x_width - 30) < 10:
+        #     x = "b"
+        # elif equity > balance + 10:
+        #     x = "c"
+        else:
+            x = "n"
+
         return x
